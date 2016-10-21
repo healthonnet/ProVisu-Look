b5bf79835c29ae1be72824d87a5147afbee146cf
Pierre Repetto-Andipatin <pierre.repetto@gmail.com>
2016-10-21 16:19:53 +0200

    fix tooltip

--- toolbar/toolbar.js
+++ toolbar/toolbar.js
@@ -42,7 +42,7 @@ function generateHTML(element, url, i18n) {
     '<li>' +
     '<a href="/service?url=' + url + '"><div class="item-normal low-vision">' +
     '</div>' +
-    '<span class="tooltip tool-normal">' + i18n.infoNormal.message +
+    '<span class="provisu-tooltip tool-normal">' + i18n.infoNormal.message +
     '</span></a>' +
     '</li>' +
 
@@ -50,7 +50,7 @@ function generateHTML(element, url, i18n) {
     '<a href="/service?url=' + url + '&filter=black">' +
     '<div class="item-black low-vision">' +
     '</div>' +
-    '<span class="tooltip tool-black">' + i18n.infoBlack.message +
+    '<span class="provisu-tooltip tool-black">' + i18n.infoBlack.message +
     '</span></a>' +
     '</li>' +
 
@@ -58,7 +58,7 @@ function generateHTML(element, url, i18n) {
     '<a href="/service?url=' + url + '&filter=blue">' +
     '<div class="item-blue low-vision">' +
     '</div>' +
-    '<span class="tooltip tool-blue">' + i18n.infoBlue.message +
+    '<span class="provisu-tooltip tool-blue">' + i18n.infoBlue.message +
     '</span></a>' +
     '</li>' +
 
@@ -66,7 +66,7 @@ function generateHTML(element, url, i18n) {
     '<a href="/service?url=' + url + '&filter=cyan">' +
     '<div class="item-cyan low-vision">' +
     '</div>' +
-    '<span class="tooltip tool-cyan">' + i18n.infoCyan.message +
+    '<span class="provisu-tooltip tool-cyan">' + i18n.infoCyan.message +
     '</span></a>' +
     '</li>' +
 
@@ -74,7 +74,7 @@ function generateHTML(element, url, i18n) {
     '<a href="/service?url=' + url + '&filter=smaller">' +
     '<div class="item-smaller">A-' +
     '</div>' +
-    '<span class="tooltip tool-normal">' + i18n.infoSmaller.message +
+    '<span class="provisu-tooltip tool-normal">' + i18n.infoSmaller.message +
     '</span></a>' +
     '</li>' +
 
@@ -82,7 +82,7 @@ function generateHTML(element, url, i18n) {
     '<a href="/service?url=' + url + '&filter=bigger">' +
     '<div class="item-bigger">A+' +
     '</div>' +
-    '<span class="tooltip tool-normal">' + i18n.infoBigger.message +
+    '<span class="provisu-tooltip tool-normal">' + i18n.infoBigger.message +
     '</span></a>' +
     '</li>' +
 
