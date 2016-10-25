92085f65e96ffde568d79cb297e484aca2741e48
Pierre REPETTO-ANDIPATIN <pierre@anthillsolutions.ch>
2016-10-25 14:00:32 +0200

    add new line of buttons (#15)

--- toolbar/toolbar.js
+++ toolbar/toolbar.js
@@ -1,6 +1,6 @@
 'use strict';
 
-function loadProvisuToolbar(element, url, file) {
+function loadProvisuToolbar(element, url, file, extra) {
   var xobj = new XMLHttpRequest();
   xobj.overrideMimeType('application/json');
   xobj.open('GET', file, true);
@@ -28,16 +28,45 @@ function loadProvisuToolbar(element, url, file) {
       infoBigger: {
         message: 'bigger',
       },
-    });
+      toolbarDescription: {
+        message: 'visually impaired persons',
+      },
+    }, extra);
   }
 
   function complete(evt) {
     var json = JSON.parse(evt.target.responseText);
-    generateHTML(element, url, json);
+    generateHTML(element, url, json, extra);
+    $(function() {
+      $('[data-toggle="tooltip"]').tooltip({
+        container: 'body',
+        template: '<div class="tooltip tooltip-big" role="tooltip">' +
+          '<div class="tooltip-arrow"></div>' +
+          '<div class="tooltip-inner tooltip-wide"></div></div>',
+      });
+      $('#toolbar-smaller').click(function() {
+        setFontSize(getFontSize() - 4);
+      });
+      $('#toolbar-bigger').click(function() {
+        setFontSize(getFontSize() + 4);
+      });
+    });
   }
 }
 
-function generateHTML(element, url, i18n) {
+function getFontSize() {
+  var el = document.body;
+  var style = window.getComputedStyle(el, null).getPropertyValue('font-size');
+  return parseFloat(style);
+}
+
+function setFontSize(size) {
+  var el = document.body;
+  el.style.fontSize = size + 'px';
+  return size;
+}
+
+function generateHTML(element, url, i18n, extra) {
   var html = '<ul id="sidebox">' +
     '<li>' +
     '<a href="/service?url=' + url + '"><div class="item-normal low-vision">' +
@@ -87,5 +116,26 @@ function generateHTML(element, url, i18n) {
     '</li>' +
 
     '</ul>';
+
+  if (extra) {
+    html = '<div class="btn-group" role="group" aria-label="...">' +
+      '<a href="/service?url=' + url + '" class="btn btn-default" ' +
+      'data-toggle="tooltip" data-placement="bottom" ' +
+      'title="' + i18n.toolbarDescription.message + '">' +
+      '<i class="fa fa-low-vision fa-2x" aria-hidden="true"></i>' +
+      '</a>' +
+      // Remove font-size buttons
+      // '<button type="button" class="btn btn-default" id="toolbar-smaller" ' +
+      // 'data-toggle="tooltip" data-placement="bottom" ' +
+      // 'title="' + i18n.infoSmaller.message + '">' +
+      // '<i class="fa fa-minus fa-2x" aria-hidden="true"></i>' +
+      // '</button>' +
+      // '<button type="button" class="btn btn-default" id="toolbar-bigger" ' +
+      // 'data-toggle="tooltip" data-placement="bottom" ' +
+      // 'title="' + i18n.infoBigger.message + '">' +
+      // '<i class="fa fa-plus fa-2x" aria-hidden="true"></i>' +
+      // '</button>' +
+      '</div>';
+  }
   document.getElementById(element).innerHTML = html;
 }
